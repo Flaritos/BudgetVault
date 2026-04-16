7f84502aabf20acdae47e72a9a421016fb9e740d
Flaritos <goldzach@me.com>
2026-04-16 14:15:46 -0400

    feat(live-activity): register BudgetActivityWidget in WidgetBundle

--- BudgetVaultWidget/BudgetVaultWidget.swift
+++ BudgetVaultWidget/BudgetVaultWidget.swift
@@ -398,6 +398,9 @@ struct BudgetVaultWidgetBundle: WidgetBundle {
         BudgetVaultMediumWidget()
         #if os(iOS)
         BudgetVaultLockScreenWidget()
+        if #available(iOS 16.2, *) {
+            BudgetActivityWidget()
+        }
         if #available(iOS 18.0, *) {
             LogExpenseControl()
         }
